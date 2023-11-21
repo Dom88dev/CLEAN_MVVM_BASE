459d496860c1a84c14458479f4c634b1d86059bd
Dom Kim <dom88.dev@gmail.com>
2023-11-21 15:24:07 +0900

    compile Sdk 변경 & lifecycleScope 에서 Job Observing 코드 수정

--- presentation/src/main/java/com/dom/presentation/screen/splash/LaunchActivity.kt
+++ presentation/src/main/java/com/dom/presentation/screen/splash/LaunchActivity.kt
@@ -2,13 +2,16 @@ package com.dom.presentation.screen.splash
 
 import android.content.Intent
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.dom.presentation.base.BaseActivity
 import com.dom.presentation.base.delegate.viewBinding
 import com.dom.presentation.databinding.ActivityLaunchBinding
 import com.dom.presentation.screen.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class LaunchActivity : BaseActivity<LaunchViewModel, ActivityLaunchBinding>() {
@@ -21,11 +24,15 @@ class LaunchActivity : BaseActivity<LaunchViewModel, ActivityLaunchBinding>() {
         }
     }
 
-    override fun observeData(): Job = lifecycleScope.launchWhenStarted {
-        vm.isReadyMainScreen.collect {
-            if (it) {
-                startActivity(Intent(this@LaunchActivity, MainActivity::class.java))
-                finish()
+    override fun observeData(): Job = lifecycleScope.launch {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            launch {
+                vm.isReadyMainScreen.collect {
+                    if (it) {
+                        startActivity(Intent(this@LaunchActivity, MainActivity::class.java))
+                        finish()
+                    }
+                }
             }
         }
     }
